d7ae9bcbd134f42af91f396a15a40b66d8a95f7e
sarawer <sarawermd@gmail.com>
2026-08-28 17:04:30 +0600

    fixed nav bar

--- src/main/java/me/sarawer/lost_and_found_campus_portal/SecurityConfig.java
+++ src/main/java/me/sarawer/lost_and_found_campus_portal/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
+                        .defaultSuccessUrl("/",true)
                         .permitAll()
                 );
 
---
+++ src/main/java/me/sarawer/lost_and_found_campus_portal/controller/AlItemController.java
@@ -0,0 +1,39 @@
+package me.sarawer.lost_and_found_campus_portal.controller;
+
+
+
+import lombok.RequiredArgsConstructor;
+import me.sarawer.lost_and_found_campus_portal.entity.FoundItem;
+import me.sarawer.lost_and_found_campus_portal.entity.LostItem;
+import me.sarawer.lost_and_found_campus_portal.service.FoundItemService;
+import me.sarawer.lost_and_found_campus_portal.service.LostItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+
+
+@RequiredArgsConstructor
+@RequestMapping("/browseItems")
+@Controller
+public class AlItemController {
+    private final FoundItemService foundItemService;
+    private final LostItemService lostItemService;
+
+    @GetMapping
+    public String browseItems(Model model) {
+
+        List<LostItem> lostItems=lostItemService.getAllLostItems();
+
+        List<FoundItem> foundItems=foundItemService.getAllFoundItems();
+
+        model.addAttribute("lostItems", lostItems);
+        model.addAttribute("foundItems", foundItems);
+
+        return "browseItems";
+    }
+}
